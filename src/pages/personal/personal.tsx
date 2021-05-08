79486d4aca3eab973990e0222305773aa849a438
ZeroY <admin@zeroy.net>
2021-05-08 16:54:47 +0800

    new

---
+++ src/pages/personal/personal.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from "@tarojs/components";
+import {AtButton} from "taro-ui";
+import "./personal.sass";
+
+const Personal = () => {
+  return (
+    <View className='personal'>
+      <Text>Hello personal!</Text>
+    </View>
+  )
+}
+
+export default Personal
